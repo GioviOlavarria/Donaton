5157ca5d9cc40156cfa486bf157f80a8620debef
lobos <t.lobos@duocuc.cl>
2026-06-17 15:53:10 -0400

    cambios web config

--- ms-beneficiarios/src/main/java/com/donaton/msbeneficiarios/WebConfig.java
+++ ms-beneficiarios/src/main/java/com/donaton/msbeneficiarios/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8082") // Your frontend URL
+                .allowedOrigins("http://localhost:3000") // Your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
