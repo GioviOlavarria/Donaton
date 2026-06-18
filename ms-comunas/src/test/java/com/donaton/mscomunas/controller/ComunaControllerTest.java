bc30270e62ebc6e12b5b0fdb6be1b3b487ebe4c2
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-06-18 16:44:25 -0400

    Arreglo de Pruebas unitarias

--- ms-comunas/src/test/java/com/donaton/mscomunas/controller/ComunaControllerTest.java
+++ ms-comunas/src/test/java/com/donaton/mscomunas/controller/ComunaControllerTest.java
@@ -1,5 +1,6 @@
 package com.donaton.mscomunas.controller;
 
+import com.donaton.mscomunas.service.ComunaService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
